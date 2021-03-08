0cb8fdfc6ab945646345c42b813f6c2995c880ab
Raimondo Ignizio <raimondoignizio@libero.it>
2021-03-08 13:12:02 +0100

    Script finished. Style finished. Needs responsive.

---
+++ counter/js/counter.js
@@ -0,0 +1,31 @@
+let counterContainer = document.getElementById("counter-container");
+
+let counter = document.createElement("p");
+counter.innerHTML = 0;
+counterContainer.append(counter);
+
+let plusButton = document.createElement("button");
+plusButton.type = "button";
+plusButton.innerHTML = "+";
+let minusButton = document.createElement("button");
+minusButton.type = "button";
+minusButton.innerHTML = "-";
+
+let buttons = document.createElement("div");
+buttons.className = "buttons-container";
+
+counterContainer.append(buttons);
+buttons.append(plusButton);
+buttons.append(minusButton);
+
+plusButton.onclick = function counterAdd() {
+  let n = Number(counter.innerHTML);
+  n++;
+  counter.innerHTML = n;
+};
+
+minusButton.onclick = function counterSubtract() {
+  let n = Number(counter.innerHTML);
+  n--;
+  counter.innerHTML = n;
+};
